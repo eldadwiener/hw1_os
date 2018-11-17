ae305737da285e4831c55aec26128ebcc76ab523
eldadwiener <eldadwiener@gmail.com>
2018-11-17 16:00:08 +0200

    first change for try

--- smash.c
+++ smash.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	/* add your code here */
 
 	/************************************/
-
+    // new commit
 	/************************************/
 	// Init globals 
 
